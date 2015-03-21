1b1ed2fc84247db6e06bd73d46ce0800b46b817c
William Lindsay <william.fr.lindsay@gmail.com>
2015-03-21 17:20:26 -0400

    - Now output valid JSON - Solved previous problem of not identifying partial models however now the following type of identification is false: model = TX10 identified with TX-100

--- src/Helpers/StringHelper.java
+++ src/Helpers/StringHelper.java
@@ -12,6 +12,10 @@ public class StringHelper {
 		return s.toLowerCase().replaceAll("[^a-z0-9]", "");
 	}
 	
+	static public String cleanStringwithSpace(String s) {
+		return s.toLowerCase().replaceAll("[^a-z0-9]", " ");
+	}
+	
 	/**
 	 * Will return true if the two strings are identical or if the LevenshteinDistance is equal to the character difference between the two
 	 * since this indicates that one simply has added characters over the other
@@ -21,10 +25,7 @@ public class StringHelper {
 	 * @return
 	 */
 	static public boolean areStringsSimilar(String s1, String s2) {
-		s1 = cleanString(s1);
-		s2 = cleanString(s2);
-		
-		return s1.compareTo(s2) == 0 || s1.contains(s2) || s2.contains(s1);
+		return contains(s1, s2) || contains(s2, s1);
 	}
 	
 	/**
@@ -35,9 +36,44 @@ public class StringHelper {
 	 * @return boolean
 	 */
 	static public boolean contains(String parent, String child) {
-		parent = cleanString(parent);
-		child = cleanString(child);
+		String cp = cleanString(parent);
+		String cc = cleanString(child);
 		
-		return parent.contains(child);
+		if (cp.length() == 0 || cc.length() == 0) {
+			return false;
+		} else if (cp.contains(cc)) {
+			return true;
+		}
+		
+		return areStringsPartiallySimilar(parent, child);
+	}
+	
+	/**
+	 * Will return true if at least 70% of the child is present in the parent with a minimum of 3 characters
+	 * 
+	 * @param parent
+	 * @param child
+	 * @return
+	 */
+	static private boolean areStringsPartiallySimilar(String parent, String child) {
+		String[] ac = cleanStringwithSpace(child).split("\\s+");
+		int matchedCharacters = 0;
+		
+		for (int i = 1; i < ac.length; i++) {
+			int curlength = 0;
+			String curString = "";
+			
+			for (int j = i; j < ac.length; j++) {
+				curlength += ac[j].length();
+				curString += ac[j];
+			}
+			
+			if (contains(parent, curString)) {
+				matchedCharacters = curlength;
+				break;
+			}
+		}
+		
+		return matchedCharacters > 3;
 	}
 }
--- src/Models/Listing.java
+++ src/Models/Listing.java
@@ -3,6 +3,8 @@ package Models;
 public class Listing {
 	private String title;
 	private String manufacturer;
+	private String currency;
+	private String price;
 	
 	public Listing() {
 		
